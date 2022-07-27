f2e0e39ce85ffd53fa4651aa8d5a5e70c90b97aa
evonicholas <66655947+evonicholas@users.noreply.github.com>
2022-07-27 22:32:41 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ import cv2
 import numpy as np
 import torch
 
+# Hardware reset (Realsense frame didnt arrive problem)
+ctx = rs.context()
+devices = ctx.query_devices()
+for dev in devices:
+    dev.hardware_reset()
+
 # YoloV5
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
 model.classes = [47, 49] # Filtering class to apple and orange
@@ -38,6 +44,9 @@ config.enable_stream(rs.stream.color, resolution_width, resolution_height, rs.fo
 
 pipeline.start(config)
 
+# Flag to run code once
+apple_detected = False
+
 # Ignoring first 5 frames for better result (Recommendation)
 # Skip 5 first frames to give the Auto-Exposure time to adjust
 for _ in range(5):
@@ -59,15 +68,29 @@ while True:
     # Object detection
     result = model(dst)
     result.render()
-    print(result.pandas().xyxy[0])
+    detected_object = result.pandas().xyxy[0]
+    obj_data = result.pandas().xyxy[0]
+    obj_name = obj_data.name[0]
+    obj_coordinates_x = int((obj_data.xmax[0] + obj_data.xmin[0])/2) # Center X
+    obj_coordinates_y = int((obj_data.ymax[0] + obj_data.ymin[0])/2) # Center Y
+
+    # Depth of detected object
+
+    # Only detect apple for now
+    if obj_name == "apple" and apple_detected == False:
+        #depth_point = rs.rs2_deproject_pixel_to_point(depth_intrin, [obj_coordinates_x, obj_coordinates_y], depth)
+        #print("Apple point = ", depth_point)
+        print("Apple center x = ", obj_coordinates_x, "Apple center y = ", obj_coordinates_y)
+        apple_detected = True
+
     
     # Show images
     cv2.imshow('frame', result.imgs[0])
-    #create exit key
+    
+    # Create exit key
     if cv2.waitKey(1) == ord('q'):
         pipeline.stop()
         break
 
 
 print("End of program")
-
